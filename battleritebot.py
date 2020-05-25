18c01867daeec42c67d73cf7c379af1d9fe2d17a
Lepnix <64446882+Lepnix@users.noreply.github.com>
2020-05-24 19:33:48 -0700

    Update battleritebot.py

--- battleritebot.py
+++ battleritebot.py
@@ -1268,7 +1268,7 @@ async def resetpb(ctx):
 
 @client.command()
 async def complaints(ctx):
-    if ctx.channel.id != MISC_COMMANDS_ID or ctx.guild != None:
+    if ctx.channel.id != MISC_COMMANDS_ID and ctx.guild != None:
         return
 
     i = 0
